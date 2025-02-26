3d5823033c7534f0d09456f7d8062d0e500c1537
DurgapoojithAdapa <durgapoojith001@gmail.com>
2025-02-26 23:28:25 +0530

    update BubbleSortingAlgo.java

--- src/com/dsa/algo/BubbleSortingAlgo.java
+++ src/com/dsa/algo/BubbleSortingAlgo.java
@@ -1,8 +1,33 @@
 package com.dsa.algo;
 
+import java.util.Arrays;
+
 public class BubbleSortingAlgo {
 
     public static void main(String[] args) {
+     int [] arr = new int[10];
+     bubbleSortAlgo(arr);
+     System.out.print(Arrays.toString(arr));
+    }
+
+    public static void bubbleSortAlgo(int[] arr){
+        boolean swapped;
+
+        for(int i =0;i<arr.length;i++){
+            swapped = false;
+            for(int j=1;j<arr.length-i;j++){
+
+                if(arr[j]< arr[j-1]){
+                    int temp =arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1] =temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped)
+                break;
+
+        }
 
     }
 }
